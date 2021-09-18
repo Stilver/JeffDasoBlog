028ba05dd44453e62fe2d7dd731cda19cdfab9fc
steven nikolaev <stilver8@gmail.com>
2021-09-18 13:03:20 +0500

    default secret key drop

--- project_core/settings.py
+++ project_core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-va8j(h4ga_&=f^ie6k&(ja9=_a0y_7yjg%-e!s*7!_csy6osp8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
